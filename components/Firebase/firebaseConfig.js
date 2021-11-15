07261aea243e1649184e96aede61a679bebf5ec7
aldrichalvarez <aldrichheinzalvarez@gmail.com>
2021-11-15 14:41:06 +0800

    update

--- components/Firebase/firebaseConfig.js
+++ components/Firebase/firebaseConfig.js
@@ -1,10 +1,11 @@
 
 export default {
-    apiKey: "AIzaSyBDWMMSgtgUd8wrBAgtb3OTPNTuTvIzQ1E",
-    authDomain: "bant-ai.firebaseapp.com",
-    projectId: "bant-ai",
-    storageBucket: "bant-ai.appspot.com",
-    messagingSenderId: "236293826086",
-    appId: "1:236293826086:web:417b47042e64e61f56a8e7",
-    measurementId: "G-LKZPDFTPKD"
-  };
+  apiKey: "AIzaSyBDWMMSgtgUd8wrBAgtb3OTPNTuTvIzQ1E",
+  authDomain: "bant-ai.firebaseapp.com",
+  databaseURL: "https://bant-ai-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "bant-ai",
+  storageBucket: "bant-ai.appspot.com",
+  messagingSenderId: "236293826086",
+  appId: "1:236293826086:web:417b47042e64e61f56a8e7",
+  measurementId: "G-LKZPDFTPKD"
+};
